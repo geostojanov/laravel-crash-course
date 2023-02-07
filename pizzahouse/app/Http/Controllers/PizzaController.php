c134960ade20d09dee38426693c572503202fc08
geostojanov <gestojanov@gmail.com>
2023-02-07 08:59:32 +0100

    controllers

---
+++ pizzahouse/app/Http/Controllers/PizzaController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PizzaController extends Controller
+{
+  public function index() {
+    // get data from a database
+    $pizzas = [
+      ['type' => 'hawaiian', 'base' => 'cheesy crust'],
+      ['type' => 'volcano', 'base' => 'garlic crust'],
+      ['type' => 'veg supreme', 'base' => 'thin & crispy']
+    ];
+
+    return view('pizzas', [
+      'pizzas' => $pizzas,
+      'name' =>  request('name'),
+      'age' =>  request('age')
+    ]);
+  }
+
+  public function show($id) {
+    return view('details', ['id' => $id]);
+  }
+}
